3ed0c0e3a80d43a0d69fcb971e01a3619fe4572b
Amel Islamovic <aislqmovic@gmail.com>
2020-05-12 19:15:13 +0200

    Changed connection to DB

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const expressLayouts = require("express-ejs-layouts")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
 
-const app = express()
+const app = express();
 
 // Database connection
-mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log("MongoDB connected..."))
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log("MongoDB connected...")).URI;
 
 // EJS
-app.use(expressLayouts)
-app.set("view engine", 'ejs')
+app.use(expressLayouts);
+app.set("view engine", "ejs");
 
 // Bodyparser
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 // Static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Routes
-app.use('', require("./routes/index"))
+app.use("", require("./routes/index"));
 
 // Port and server starting
-PORT = 5000 || process.env.PORT
-app.listen(PORT, () => console.log(`Server started on port ${PORT}...`))
+PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
